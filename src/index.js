7b152340f531930bf88bb83dfdfd0dbd6b6f0c05
victor <147457072+victorsancar@users.noreply.github.com>
2025-09-17 11:00:03 -0300

    Update index.js
    ateração no index, import ./firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // Importa o componente principal
-import './components/firebase'; // Importa e executa o arquivo de inicialização do Firebase
+import './firebase'; // Importa e executa o arquivo de inicialização do Firebase
 
 // Cria a raiz da aplicação no elemento com id 'root' do HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
